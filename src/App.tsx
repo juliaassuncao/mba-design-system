16a1d20f49fc8655cfe99f0460a92536f6700ff2
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-13 11:39:16 -0300

    feat: adicionar componente Form com input e botão estilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
+import { Form } from "./components/Form"
 
 function App() {
 
   return (
     <div className={styles.container}>
       <Header />
+      <Form />
     </div>
   )
 }
