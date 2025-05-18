c07c68d14d51e6145bb4630fb1ee6a74a2f01409
Julia Assunção <juliaassuncao256@gmail.com>
2025-05-18 20:05:56 -0300

    feat: ajustes finais no DS

--- src/DS/spacing/index.ts
+++ src/DS/spacing/index.ts
@@ -1,8 +1,10 @@
 export const DS_Spacing = {
+  s08: "8px",
   s12: "12px",
   s16: "16px",
   s18: "18px",
   s36: "36px",
   s42: "42px",
-  s60: "60px"
+  s48: "48px",
+  s60: "60px",
 }
--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { DS_Colors } from "./DS/colors";
+import { DS_Spacing } from "./DS/spacing";
 
 export const Container = styled.div`
   max-width: 627px;
   margin: 0 auto;
-  padding: 36px;
+  padding: ${DS_Spacing.s36};
   display: flex;
   flex-direction: column;
 `;
@@ -17,32 +18,33 @@ export const Logo = styled.div`
   margin-bottom: 3.68rem;
 
   img {
-    width: 2rem;
+    width: 32px;
   }
 
   span {
-    font-size: 2rem;
+    font-size: 32px;
+    line-height: 40px;
     font-weight: 700;
     color: ${DS_Colors.content.secondary};
   }
 `;
 
 export const Title = styled.h1`
-  font-size: 1.5rem;
+  font-size: 24px;
   line-height: 32px;
   color: ${DS_Colors.content.primary};
 `;
 
 export const Form = styled.form`
   display: flex;
-  gap: 16px;
-  margin: 42px 0;
+  gap: ${DS_Spacing.s16};
+  margin: ${DS_Spacing.s42} 0;
 `;
 
 export const ItemsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 12px;
+  gap: ${DS_Spacing.s12};
   max-height: 232px;
   overflow-y: auto;
   position: relative;
@@ -61,7 +63,6 @@ export const ItemsContainer = styled.div`
     position: absolute;
     top: 0;
     right: 0;
-    width: 8px;
     height: 100%;
     background: ${DS_Colors.background.secundary};
     pointer-events: none;
